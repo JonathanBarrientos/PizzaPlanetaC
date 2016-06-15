d82e305be6e3b36245ee01227c376ca23ca872dd
fixt <20113036@aloe.ulima.edu.pe>
2016-06-15 13:55:37 -0500

    Validaciones

--- app/src/main/java/com/ulima/sw/pizzaplanetac/listado/ListadoPizzasPresenterImp.java
+++ app/src/main/java/com/ulima/sw/pizzaplanetac/listado/ListadoPizzasPresenterImp.java
@@ -28,33 +28,6 @@ public class ListadoPizzasPresenterImp implements ListadoPizzasPresenter {
     }
 
 
-    public void obtenerListaP2() {
-
-      /*  List<Pedido> pedidos = new ArrayList<>();
-
-        List<Pizza> pizzas = new ArrayList<>();
-        Pizza pi = new Pizza();
-        Pizza p1 = new Pizza();
-
-        pi.setId(1);
-        pi.setNombre("Americana");
-        pi.setTamaño("M");
-
-        pi.setImg("@drawable/americana");
-
-        pizzas.add(pi);
-        p1.setId(2);
-        p1.setNombre("Bacon");
-        p1.setTamaño("F");
-        p1.setImg("@drawable/bacon");
-
-        pizzas.add(p1);
-
-
-
-       lview.mostrarPizzas(pizzas);*/
-    }
-
     @Override
     public void obtenerListaP(int idpizzas) {
 
